67273bcb578627d1989f32629a314290a15380d7
Haseeb Ali <haseebali1@gmail.com>
2020-01-04 06:24:48 -0800

    made img folder and moved ship image to it

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -13,7 +13,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect.
-        self.image = pygame.image.load('ship.bmp')
+        self.image = pygame.image.load('img/ship.bmp')
         self.rect = self.image.get_rect()
 
         # start each new ship at the bottom center of the screen
