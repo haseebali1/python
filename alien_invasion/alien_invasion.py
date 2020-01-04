78d7b9f779207c8b7fdc81f19bbddd37bbfa727c
Haseeb Ali <haseebali1@gmail.com>
2020-01-04 09:02:56 -0800

    added pressing q to quit the game

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -61,6 +61,9 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             # Move the ship to the left
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            # quit the game if q is pressed
+            sys.exit()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
