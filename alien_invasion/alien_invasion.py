822a460e003a8fd57d3d2f7e7958b0ffc875541e
Haseeb Ali <haseebali1@gmail.com>
2020-01-05 06:08:48 -0800

    changed p starting the game to the enter key

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -111,7 +111,7 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
-        elif event.key == pygame.K_p:
+        elif event.key == pygame.K_RETURN:
             self._start_game()
 
     def _check_keyup_events(self, event):
