0a20733119d238510e391eae3d474adb76f937bb
Haseeb Ali <haseebali1@gmail.com>
2020-01-05 09:08:25 -0800

    made changes to score keeping for missing scoring on hitting multiple aliens at the same time

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -162,7 +162,9 @@ class AlienInvasion:
                 True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += \
+                self.settings.alien_points * len(aliens)
             self.sb.prep_score()
 
         if not self.aliens:
