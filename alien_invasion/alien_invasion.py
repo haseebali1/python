aab2c25026a7d3993bd4fe5d601c9b1a37468e0f
Haseeb Ali <haseebali1@gmail.com>
2020-01-05 06:07:41 -0800

    refactored check_play button code to a new helper method start game and added starting the game by pressing p

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -75,19 +75,22 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
-            self.stats.reset_stats()
-            self.stats.game_active = True
+            self._start_game()
 
-            #Get rid of any remaining aliens and bullets.
-            self.aliens.empty()
-            self.bullets.empty()
+    def _start_game(self):
+        self.stats.reset_stats()
+        self.stats.game_active = True
 
-            #Create a new fleet and center the ship.
-            self._create_fleet()
-            self.ship.center_ship()
+        #Get rid of any remaining aliens and bullets.
+        self.aliens.empty()
+        self.bullets.empty()
 
-            #hide the mouse cursor
-            pygame.mouse.set_visible(False)
+        #Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        #hide the mouse cursor
+        pygame.mouse.set_visible(False)
 
     #Helper Methods
     def _check_keydown_events(self, event):
@@ -108,6 +111,8 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+        elif event.key == pygame.K_p:
+            self._start_game()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
