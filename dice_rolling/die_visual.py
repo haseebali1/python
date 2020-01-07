4e0d0fd2769e402bd1794b615c3adc5f86d1da95
Haseeb Ali <haseebali1@gmail.com>
2020-01-07 11:58:29 -0800

    added multiple dice to be thrown

--- dice_rolling/die_visual.py
+++ dice_rolling/die_visual.py
@@ -1,18 +1,34 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 #create a six-sided die
-die = Die()
+die1 = Die()
+die2 = Die()
 
 # make some reolls and store results in a list
-results = []
+results = [ die1.roll() + die2.roll() for value in range(1000)]
 
-for roll_num in range(100):
-    results.append(die.roll())
+#for roll_num in range(1000):
+#    results.append(die1.roll() + die2.roll())
 
 #analyzing the results
-frequencies = []
-for value in range(1, die.num_sides + 1):
-    frequencies.append(results.count(value))
-print(results)
+max_result = die1.num_sides + die2.num_sides
+frequencies = [results.count(value) for value in range(2,max_result+1)]
+#for value in range(2, max_result + 1):
+#    frequencies.append(results.count(value))
 
 print(frequencies)
+
+# Visualize the results
+x_values = list(range(2,max_result+1))
+data = [Bar(x=x_values,y=frequencies)]
+
+# dtick label every bar
+x_axis_config = {'title' : 'Result', 'dtick' : 1}
+y_axis_config = {'title' : 'Frequency of Result'}
+
+my_layout = Layout(title='Results of rolling two D6 1000 times',
+        xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data' : data, 'layout':my_layout}, filename='d6_d6.html')
