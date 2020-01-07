6f305723f316df23fd0d5d040ca9f97f9ff4863f
Haseeb Ali <haseebali1@gmail.com>
2020-01-07 13:47:33 -0800

    got all the high temperatures from the file

--- weather_visualization/sitka_highs.py
+++ weather_visualization/sitka_highs.py
@@ -3,6 +3,12 @@ import csv
 filename = 'data/sitka_weather_07-2018_simple.csv'
 
 with open(filename) as f:
+    # creates a reader object
     reader = csv.reader(f)
+    # next returns the next line in the file
     header_row = next(reader)
-    print(header_row)
+
+    # high temps are in column 5
+    highs = [int(row[5]) for row in reader]
+
+print(highs)
