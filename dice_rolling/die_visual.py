86fafedf843fe16635f8c7aff6a159694ded6ef0
Haseeb Ali <haseebali1@gmail.com>
2020-01-06 19:59:10 -0800

    count the frequency that each side of the dice rolled

--- dice_rolling/die_visual.py
+++ dice_rolling/die_visual.py
@@ -9,4 +9,10 @@ results = []
 for roll_num in range(100):
     results.append(die.roll())
 
+#analyzing the results
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequencies.append(results.count(value))
 print(results)
+
+print(frequencies)
