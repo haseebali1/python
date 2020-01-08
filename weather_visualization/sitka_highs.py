ce6e1606b40a9a5817b66d0a4b3e053fca44c321
Haseeb Ali <haseebali1@gmail.com>
2020-01-07 17:09:06 -0800

    got the low temperatures for comparison and added them to the plot

--- weather_visualization/sitka_highs.py
+++ weather_visualization/sitka_highs.py
@@ -3,7 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 
 with open(filename) as f:
     # creates a reader object
@@ -11,19 +11,21 @@ with open(filename) as f:
     # next returns the next line in the file
     header_row = next(reader)
 
-    highs, dates = [], []
-    # high temps are in column 5 and date is in column 2
+    highs, lows, dates = [], [], []
+    # high temps are in column 5 and date is in column 2, and low temps in 6
     for row in reader:
         dates.append(datetime.strptime(row[2], '%Y-%m-%d'))
         highs.append(int(row[5]))
+        lows.append(int(row[6]))
 
 # plot the high temperatures.
-plt.style.use('dark_background')
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c = 'red')
+ax.plot(dates, lows, c = 'blue')
 
 #format plot
-plt.title("Daily high temperatures, july 2018", fontsize=24)
+plt.title("Daily High and Low Temperatures - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
