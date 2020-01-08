8594850a3d83c1c04e30c89ae02502b4e252199a
Haseeb Ali <haseebali1@gmail.com>
2020-01-07 17:01:18 -0800

    got the dates for the temperatures and used them for the x values

--- weather_visualization/sitka_highs.py
+++ weather_visualization/sitka_highs.py
@@ -11,17 +11,21 @@ with open(filename) as f:
     # next returns the next line in the file
     header_row = next(reader)
 
-    # high temps are in column 5
-    highs = [int(row[5]) for row in reader]
+    highs, dates = [], []
+    # high temps are in column 5 and date is in column 2
+    for row in reader:
+        dates.append(datetime.strptime(row[2], '%Y-%m-%d'))
+        highs.append(int(row[5]))
 
 # plot the high temperatures.
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
-ax.plot(highs, c = 'red')
+ax.plot(dates, highs, c = 'red')
 
 #format plot
 plt.title("Daily high temperatures, july 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
