ad21fdd221eeb3917d97d2c472c9939be59210f8
Haseeb Ali <haseebali1@gmail.com>
2020-01-07 16:50:39 -0800

    instead of printing the high temp list plotted the temperatures

--- weather_visualization/sitka_highs.py
+++ weather_visualization/sitka_highs.py
@@ -1,5 +1,8 @@
 import csv
 
+import matplotlib.pyplot as plt
+from datetime import datetime
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 
 with open(filename) as f:
@@ -11,4 +14,15 @@ with open(filename) as f:
     # high temps are in column 5
     highs = [int(row[5]) for row in reader]
 
-print(highs)
+# plot the high temperatures.
+plt.style.use('dark_background')
+fig, ax = plt.subplots()
+ax.plot(highs, c = 'red')
+
+#format plot
+plt.title("Daily high temperatures, july 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
