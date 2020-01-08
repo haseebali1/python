d0b26c453eb3aa3a194bdc40cb32f4e2b95657f3
Haseeb Ali <haseebali1@gmail.com>
2020-01-08 14:12:49 -0800

    got 30 articles, and also added try-except for when 'descendants' key was not found

--- hacker_news_api/hn_article.py
+++ hacker_news_api/hn_article.py
@@ -1,13 +1,42 @@
+from operator import itemgetter
 import requests
 import json
 
 #Make an api call, and store the responses
-url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
 
-#explore the structure of the data
-response_dict = r.json()
-readable_file = 'data/readable_hn_data.json'
-with open(readable_file, 'w') as f:
-    json.dump(response_dict, f, indent=4)
+# process information about each submission.
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:30]:
+    # make a seperate API call for each submission.
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+    r = requests.get(url)
+    print(f"id: {submission_id}\tstatus: {r.status_code}")
+    response_dict = r.json()
+
+    # build a dictionary for each article
+    try:
+        comments = response_dict['descendants']
+    except KeyError:
+        pass
+    else:
+        title = response_dict['title'],
+        hn_link = f"http://news.ycombinator.com/item?id={submission_id}",
+        submission_dict = {
+                    'title' : title,
+                    'hn_link' : hn_link,
+                    'comments' : comments
+                }
+        submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts,
+        key=itemgetter('comments'),
+        reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"\nDiscussion link: {submission_dict['hn_link']}")
+    print(f"\nComments: {submission_dict['comments']}")
