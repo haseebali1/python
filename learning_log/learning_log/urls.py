fd459a5e53d5ded602c68aa5f170f260bf0bdfe2
Haseeb Ali <haseebali1@gmail.com>
2020-01-09 07:33:59 -0800

    added the home page for the site

--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from . import views
-
-app_name = 'learning_logs'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Home Page
-    path('',views.index, name='index')
+    path('', include('learning_logs.urls'))
 ]
