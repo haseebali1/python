2e30852eb74353fa3137252e18d3c601cab510fb
Haseeb Ali <haseebali1@gmail.com>
2020-01-09 09:57:33 -0800

    added base template and modified index to extend from base and created topics to show topics

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -7,5 +7,9 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
         #Home Page
-        path('', views.index, name='index')
+        path('', views.index, name='index'),
+        #page that shows all the topics
+        path('topics/', views.topics, name='topics'),
+        #detail page for a single topic
+        path('topics/<int:topic_id>/', views.topic, name ='topic')
 ]
